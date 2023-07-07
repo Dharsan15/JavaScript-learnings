c267401038d7f72637be086fd411d840d2ba91d1
Dharsan15 <dharsanravi6677@gmail.com>
2023-07-07 22:03:11 +0530

    added 1st question

--- Destructuring-practice.js
+++ Destructuring-practice.js
@@ -8,3 +8,20 @@ console.log(an1);
 console.log(an2);
 
 //question : 1
+//object destructuring syntax 
+
+const snacks = {
+    chips : {name : 'Pringles' , salty : true},
+    candy : {name : 'twizzlers' , salty : false},
+    chocolate : {name : 'mars' , salty : false}
+ }
+ 
+
+ //In destructuring objects the key should match the objects key
+ const {candy , fruit = {name : 'banana' , salty : false}} = snacks;
+ 
+ console.log(candy , fruit);
+ 
+ 
+ 
+ 
