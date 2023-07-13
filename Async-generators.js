9743eccd730ac7a3697cc7395a05ff27a2bf6d51
Dharsan15 <dharsanravi6677@gmail.com>
2023-07-13 21:56:06 +0530

    added 1st question

--- Async-generators.js
+++ Async-generators.js
@@ -25,5 +25,37 @@ const asyncgenerator = async function* () {
 
   // question : 1
 
+  // generator function which return either of two promises of which one resolve in .5 sec
+  // and 3 sec and it should run infinetly
+
+  const asyncGenfun = async function* () {
+    while (1) {
+      const val = Math.floor(Math.random() * 2);
+  
+      if (val == 0) {
+        yield new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve("Fast");
+          }, 500);
+        });
+      } else {
+        yield new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve("slow");
+          }, 3000);
+        });
+      }
+    }
+  };
+  
+  const asyncGen = asyncGenfun();
+  
+  async function asyncfun() {
+    for await (const promise of asyncGen) {
+      console.log(promise);
+    }
+  }
+  
+  asyncfun();
   
 
