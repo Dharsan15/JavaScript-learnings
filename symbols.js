d987bd329411033eede58cd6033c3dad8ae2e0c4
Dharsan15 <dharsanravi6677@gmail.com>
2023-07-15 16:35:15 +0530

    1st question added

--- symbols.js
+++ symbols.js
@@ -8,4 +8,28 @@ console.log(typeof gorilla);
 console.log();
 
 // question : 1
-// 
+
+// creating own iterator to the object using symbol
+
+const book = {
+    name: "1984",
+    author: "George orwell",
+    year: 1948,
+    rating: 4.5,
+    genre: "science friction",
+    movie: true,
+    [Symbol.iterator]: function* () {
+      const entries = Object.entries(book);
+  
+      for (const entry of entries) {
+        yield entry;
+      }
+    },
+  };
+  
+  for (const val of book) {
+    console.log(val);
+  }
+  
+
+
