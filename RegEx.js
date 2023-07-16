be403d8fdec42f0b263a5e241e58fe5612671b02
Dharsan15 <dharsanravi6677@gmail.com>
2023-07-16 11:53:47 +0530

    added 2nd question

--- RegEx.js
+++ RegEx.js
@@ -10,3 +10,15 @@ let match = str.match(pattern)
 console.log(test);
 
 console.log(match);
+
+//question : 1
+
+// checking of a .com on the end of a string to verify email
+
+const email = 'monkey@tress.com';
+
+const emailPattern = /.com$/;
+
+const match = email.match(emailPattern)
+
+console.log(match);
