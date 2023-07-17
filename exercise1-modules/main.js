2452764e4c05bae1e92bc462991a1eb210c5f3a6
Dharsan15 <dharsanravi6677@gmail.com>
2023-07-17 23:37:16 +0530

    added 1st exercise

---
+++ exercise1-modules/main.js
@@ -0,0 +1,5 @@
+import { capsLock } from "./utils.js";
+
+const val = capsLock('dharsan');
+
+console.log(val);
---
+++ exercise1-modules/utils.js
@@ -0,0 +1,5 @@
+function capsLock(string) {
+    return string.toUpperCase();
+}
+
+export { capsLock }
