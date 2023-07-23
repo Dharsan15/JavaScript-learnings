72803a89ffaa259b1b3a026685e3fbf3968a4cfa
Dharsan15 <dharsanravi6677@gmail.com>
2023-07-23 23:35:34 +0530

    added comments

--- fetchApi.js
+++ fetchApi.js
@@ -3,7 +3,7 @@
 // request a webpage and print the page source
 
 fetch("https://github.com/Dharsan15/To-Do-App.git")
-.then((response) => response.text())
+.then((response) => response.json())
 .then((html) => 
 console.log(html)
 
@@ -12,13 +12,13 @@ console.log(html)
 //question : 1
 
 // this url just return a json 
+// we can use .json method to get a json otherwise 
+//it returns a string
 
 fetch("https:/jsonplaceholder.typicode.com/posts/1")
 .then((res) =>
 {
     return res.text();
-   
-
 }
 )
 
