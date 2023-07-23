2deedf472d217f740d69f8c18be5762eec545669
Dharsan15 <dharsanravi6677@gmail.com>
2023-07-23 23:26:28 +0530

    added 2nd question

--- fetchApi.js
+++ fetchApi.js
@@ -2,10 +2,25 @@
 
 // request a webpage and print the page source
 
-fetch("https://github.com/Dharsan15/To-Do-App.git")
-.then((response) => response.text())
-.then((html) => 
-console.log(html)
+// fetch("https://github.com/Dharsan15/To-Do-App.git")
+// .then((response) => response.text())
+// .then((html) => 
+// console.log(html)
 
+// )
+
+//question : 1
+
+
+fetch("https:/jsonplaceholder.typicode.com/posts/1")
+.then((res) =>
+{
+    return res.text();
+   
+
+}
 )
 
+.then((text) => {
+    console.log(text);
+})
