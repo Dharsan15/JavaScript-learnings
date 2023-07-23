beed9892143bd618d50a9148cea5d41c0bdfdd9b
Dharsan15 <dharsanravi6677@gmail.com>
2023-07-23 23:31:20 +0530

    minor changes

--- fetchApi.js
+++ fetchApi.js
@@ -2,12 +2,12 @@
 
 // request a webpage and print the page source
 
-// fetch("https://github.com/Dharsan15/To-Do-App.git")
-// .then((response) => response.text())
-// .then((html) => 
-// console.log(html)
+fetch("https://github.com/Dharsan15/To-Do-App.git")
+.then((response) => response.text())
+.then((html) => 
+console.log(html)
 
-// )
+)
 
 //question : 1
 
@@ -25,5 +25,7 @@ fetch("https:/jsonplaceholder.typicode.com/posts/1")
 .then((text) => {
     console.log(text);
 })
+
+//question : 2
 
 
