5643d777579c34c48e94b4cb80234fe4744dd1c6
Dharsan15 <dharsanravi6677@gmail.com>
2023-07-23 23:17:35 +0530

    added 1st question

--- fetchApi.js
+++ fetchApi.js
@@ -1,4 +1,11 @@
 // question : 0
 
+// request a webpage and print the page source
 
+fetch("https://github.com/Dharsan15/To-Do-App.git")
+.then((response) => response.text())
+.then((html) => 
+console.log(html)
+
+)
 
