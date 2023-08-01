d957af0c59430e0cb6952156c3d8a6f2c24347be
Dharsan15 <dharsanravi6677@gmail.com>
2023-08-01 18:57:35 +0530

    added comment

--- spread-op-practise.js
+++ spread-op-practise.js
@@ -14,6 +14,7 @@ console.log(arrCopy);
 // question : 1
 // getting more on copying the iterables
 
+
 const arr = [[10,20,30] , [40,50,60]]
 
 const arrCopy = [...arr];
@@ -22,6 +23,8 @@ arrCopy[0].pop();
 arrCopy[1][0] = 80;
 
 // this doesnt reflect in arr because we are adding a new value not modiying the existing values
+
+// here we are putting a value in copy not on the original array
 arrCopy.push([9,9]);
 console.log(arr);
 console.log(arrCopy);
