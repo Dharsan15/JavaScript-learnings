4256712af3eeb3fe947bd6242038a181b7ee444c
Dharsan15 <dharsanravi6677@gmail.com>
2023-08-22 22:24:02 +0530

    added example

--- recursion.js
+++ recursion.js
@@ -1,1 +1,16 @@
 // calling the same function inside itself is called recursion 
+
+function add(a)
+{
+    if(a == 5)
+    {
+        return a;
+    }
+    
+    add(a + 1);
+
+    console.log(a);
+
+}
+
+add(0);
